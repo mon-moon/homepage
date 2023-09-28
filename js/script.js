f561c5304973f1b70b0b684ba5519a07826f414e
Monika Sawicka <monik.sawicka@gmail.com>
2023-09-28 14:59:03 +0200

    code format

--- js/script.js
+++ js/script.js
@@ -1,54 +1,40 @@
 console.log("Hejka ");
 
-let darkmode__button = document.querySelector(".darkmode__button");
-let body = document.body;
+const darkmode__button = document.querySelector(".darkmode__button");
+const body = document.body;
 
 darkmode__button.addEventListener("click", () => {
-    const isDarkMode = body.classList.contains("nav--darkMode");
-    
-    if (isDarkMode) {
-        body.classList.remove("nav--darkMode");
-        darkmode__button.querySelector("img").setAttribute("src", "https://seopuzle.pl/wp-content/uploads/2023/09/light_mode.png");
-    } else {
-        body.classList.add("nav--darkMode");
-        darkmode__button.querySelector("img").setAttribute("src", "https://seopuzle.pl/wp-content/uploads/2023/09/dark_mode.png");
-    }
+  const isDarkMode = body.classList.contains("nav--darkMode");
+
+  if (isDarkMode) {
+    body.classList.remove("nav--darkMode");
+    darkmode__button
+      .querySelector("img")
+      .setAttribute(
+        "src",
+        "https://seopuzle.pl/wp-content/uploads/2023/09/light_mode.png"
+      );
+  } else {
+    body.classList.add("nav--darkMode");
+    darkmode__button
+      .querySelector("img")
+      .setAttribute(
+        "src",
+        "https://seopuzle.pl/wp-content/uploads/2023/09/dark_mode.png"
+      );
+  }
 });
 
-
-
-let callme = document.querySelector(".button");
+const callme = document.querySelector(".button");
 console.log(callme);
 
 callme.addEventListener("click", () => {
-    callme.innerHTML = "+48 000 000 000";
-})
+  callme.innerHTML = "+48 000 000 000";
+});
 
-let emailme = document.querySelector(".button--sendemail");
-console.log(emailme)
+const emailme = document.querySelector(".button--sendemail");
+console.log(emailme);
 
 emailme.addEventListener("click", () => {
-    emailme.innerHTML = "taktak@gmail.com";
-})
-
-
-
-
-
-let myNumber = 5;
-
-let double = number => {
-    console.log(`myNumber wynosi ${myNumber}`)
-    return number * 2;
-};
-
-
-
-
-
-
-
-function nazwa(parametr1, parametr2) {
-    // instrukcje
-    return wartość; 
-}
+  emailme.innerHTML = "taktak@gmail.com";
+});
