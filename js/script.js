757f88ba564875bcae7390d6d5d6727983c7b342
Monika Sawicka <monik.sawicka@gmail.com>
2023-09-28 14:34:46 +0200

    update to css bem style

--- js/script.js
+++ js/script.js
@@ -2,35 +2,53 @@ console.log("Hejka ");
 
 let darkmode__button = document.querySelector(".darkmode__button");
 let body = document.body;
-let nav =document.nav;
 
 darkmode__button.addEventListener("click", () => {
     const isDarkMode = body.classList.contains("nav--darkMode");
     
     if (isDarkMode) {
         body.classList.remove("nav--darkMode");
-        nav.classList.remove("nav--darkMode");
         darkmode__button.querySelector("img").setAttribute("src", "https://seopuzle.pl/wp-content/uploads/2023/09/light_mode.png");
     } else {
         body.classList.add("nav--darkMode");
-        nav.classList.add("nav--darkMode");
         darkmode__button.querySelector("img").setAttribute("src", "https://seopuzle.pl/wp-content/uploads/2023/09/dark_mode.png");
     }
 });
 
 
 
-let callme = document.querySelector(".article__callmebutton");
+let callme = document.querySelector(".button");
 console.log(callme);
 
 callme.addEventListener("click", () => {
     callme.innerHTML = "+48 000 000 000";
 })
 
-let emailme = document.querySelector(".article__emailmebutton");
+let emailme = document.querySelector(".button--sendemail");
 console.log(emailme)
 
 emailme.addEventListener("click", () => {
     emailme.innerHTML = "taktak@gmail.com";
 })
 
+
+
+
+
+let myNumber = 5;
+
+let double = number => {
+    console.log(`myNumber wynosi ${myNumber}`)
+    return number * 2;
+};
+
+
+
+
+
+
+
+function nazwa(parametr1, parametr2) {
+    // instrukcje
+    return wartość; 
+}
